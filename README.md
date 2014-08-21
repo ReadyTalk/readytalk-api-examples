ddfa0482d700e867850db3afba6bb4df3107254b
Casey Watson <watsoncj@gmail.com>
2014-08-21 16:34:56 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# ReadyTalk API Examples
+
+For more information see the [ReadyTalk API Documentation](https://cc.readytalk.com/api/1.3/rest).
+
+## Setup Login Information
+
+To run the examples, first export these variables:
+
+    export TOLLFREE=8661234567
+    export ACCESSCODE=1234567
+    export PASSCODE=1234
+
+## Run Examples
+
+    ./curl/create-on-demand-meeting.sh
---
+++ curl/create-on-demand-meeting.sh
@@ -0,0 +1,14 @@
+HOST="apidev-cc.readytalk.com"
+curl "https://$HOST/api/1.3/svc/rs/meetings.json" \
+       --user "$TOLLFREE:$ACCESSCODE:$PASSCODE" \
+       -H 'Content-Type: application/x-www-form-urlencoded' \
+       --data "title=Test&\
+hostName=Chip%20Jackson&\
+fromEmail=chip.jackson%40readytalk.com&\
+timeZone=America%2FDenver&\
+startDateIso8601=2015-11-02T15:02:00.000-06:00&\
+durationInSeconds=1000&\
+registration=REG_AT_TIME_AUTOMATIC_CONFIRMATION_NO_NOTIFICATION&\
+type=WEB_AND_AUDIO&\
+audio.ondemand=DISPLAY_TOLLFREE_DISPLAY_TOLL"
+
