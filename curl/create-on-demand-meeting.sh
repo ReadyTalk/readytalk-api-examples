3bffa6e905608b368715de45abdb2a45c004484c
Casey Watson <casey.watson@readytalk.com>
2016-03-22 09:52:56 -0600

    bump date

--- curl/create-on-demand-meeting.sh
+++ curl/create-on-demand-meeting.sh
@@ -1,3 +1,4 @@
+set -x
 HOST="apidev-cc.readytalk.com"
 curl "https://$HOST/api/1.3/svc/rs/meetings.json" \
        --user "$TOLLFREE:$ACCESSCODE:$PASSCODE" \
@@ -6,7 +7,7 @@ curl "https://$HOST/api/1.3/svc/rs/meetings.json" \
 hostName=Chip%20Jackson&\
 fromEmail=chip.jackson%40readytalk.com&\
 timeZone=America%2FDenver&\
-startDateIso8601=2015-11-02T15:02:00.000-06:00&\
+startDateIso8601=2017-11-02T15:02:00.000-06:00&\
 durationInSeconds=1000&\
 registration=REG_AT_TIME_AUTOMATIC_CONFIRMATION_NO_NOTIFICATION&\
 type=WEB_AND_AUDIO&\
